17228569ec2ac8daed9544656280549c1fb0cfb8
murad <muradkv@gmail.com>
2019-05-20 20:39:19 +0300

    Add PersonCell

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by murad on 20/05/2019.
+//  Copyright © 2019 murad. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
