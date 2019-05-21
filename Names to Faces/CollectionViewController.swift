c05616ee79042cc0ddf4debadd5cc9001969dff7
murad <muradkv@gmail.com>
2019-05-21 22:04:32 +0300

    Write(copy) selected image from library to app's document path

--- Names to Faces/CollectionViewController.swift
+++ Names to Faces/CollectionViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
 
     }
 
@@ -43,4 +45,35 @@ class CollectionViewController: UICollectionViewController {
         return cell
     }
 
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    //MARK: UIImagePickerControllerDelegate methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //extract the image from the dictionary that is passed as a parameter.
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        //generate a unique filename for every image we import
+        let imageName = UUID().uuidString
+        //generate full unique path to our file(image) on App's document directory
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        //write(copy) image to our App's disk
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
